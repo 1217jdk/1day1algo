4508ab74265f5234fa6807482a893409d6d8a98f
sawol <55649302+sawol@users.noreply.github.com>
2023-01-14 19:49:15 +0900

    Update BOJ_5557.py

--- BOJ/5557/BOJ_5557.py
+++ BOJ/5557/BOJ_5557.py
@@ -1,9 +1,27 @@
 import sys
+
 read = sys.stdin.readline
 
 n = int(read())
-arr = list(map(int, read().split()))
-dp = [0] * (n)
 
-# for i in range(n):
-#     dp[i] = max(arr[i] + dp[i-1], d
+arr = list(map(int, read().split()))
+
+dp = [[0] * 21 for _ in range(n)]
+
+dp[0][arr[0]] = 1
+
+for i in range(1, n-1):
+
+  for j in range(21):
+
+    if dp[i-1][j]:
+
+      if j + arr[i] <= 20:
+
+        dp[i][j+arr[i]] += dp[i-1][j]
+
+      if j - arr[i] >= 0:
+
+        dp[i][j-arr[i]] += dp[i-1][j]
+
+print(dp[n-2][arr[n-1]])
